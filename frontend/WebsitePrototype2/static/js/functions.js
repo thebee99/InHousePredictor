4355a99410d86955ecef9037c09f978d19db6bab
Lanten501st <132638384+Lanten501st@users.noreply.github.com>
2023-05-26 14:13:37 +0900

    prototype containing php file

--- frontend/WebsitePrototype2/static/js/functions.js
+++ frontend/WebsitePrototype2/static/js/functions.js
@@ -53,18 +53,6 @@
     mainbutton.innerText = label + myName;
   }
 
-  function showDialog(text) {
-    var dialogBox = document.getElementById("dialogBox");
-    var dialogText = document.getElementById("dialogText");
-    dialogText.textContent = text;
-    dialogBox.style.display = "block";
-  }
-  
-  function hideDialog() {
-    var dialogBox = document.getElementById("dialogBox");
-    dialogBox.style.display = "none";
-  }
-
 
   function myFunction1(myDropdown, buttonNumber, description, initializer, final, description1, input) {
     
@@ -80,51 +68,58 @@
     }
   }
 
-  function readFile(path,myDropdown, buttonNumber, description, description1, input){
+  function readFile(myDropdown, buttonNumber, description, description1, input){
+
+    var lines = [". Jl. Bekasi Raya", ". Jl. Lingkar Luar" , "Ampera","Ancol","Angke", "Antasari", "Asemka","Bandengan","Bangka","Bekasi","Bendungan Hilir","Bintaro","Buaran","Buncit"
+    ,"Cakung","Cawang","Cempaka Putih","Cengkareng","Cibubur","Cideng","Ciganjur","Cijantung","Cilandak","Cilangkap","Cililitan","Cilincing","Cinere","Cipayung","Cipedak"
+    ,"Cipete","Cipinang","Ciputat","Ciputat Timur","Ciracas","Cirendeu","Citra Garden","Condet","Daan Mogot","Dewi Sartika","Duren Sawit","Duren Tiga","Duri Kepa","Duri Kosambi","Duri Pulo"
+    ,"Fatmawati","Gambir","Gandaria","Gatot Subroto","Gelong","Glodok","Green Garden","Green Ville","Grogol Petamburan","Gunung Sahari","Halim","Jagakarsa","Jakarta Garden City/JGC","Jalan Panjang","Jati Padang"
+    ,"Jatinegara","Jatiwaringin","Jelambar","Jembatan Dua","Jembatan Lima","Jembatan Tiga","Jl. Agung Perkasa","Jl. Bekasi Raya","Jl. Daan Mogot Raya","Jl. Griya Agung","Jl. H. Saaba","Jl. Joglo Raya"
+    ,"Jl. Kembangan Raya","Jl. Meruya Selatan","Jl. Permata Palem Raya","Jl. Peta Barat","Jl. Sunter Paradise","Jl. Taman Palem Lestari","Jl. Taman Sunter Indah","Jl. Tanjung Pura","Jl. Utama III","Joglo","Johar Baru","Kali Deres"
+    ,"Kalibata","Kalimalang","Kalisari","Kamal","Kampung Ambon","Kampung Rambutan","Kapuk",'Kapuk Muara','Kayu Jati','Kayu Putih','Kebagusan','Kebayoran Baru','Kebayoran Lama','Kebon Jeruk'
+    ,'Kedoya','Kel. Sunter Agung','Kelapa Dua','Kelapa Gading','Kemang','Kemanggisan','Kemayoran','Kembangan','Kepa Duri','Klender','Koja','Kota','Kota Wisata Cibubur'
+    ,'Kramat Jati','Kuningan','Lebak Bulus','Lenteng Agung','Lubang Buaya','Makasar','Mampang Prapatan','Mangga Besar','Mangga Dua','Manggarai','Matraman','Menteng','Meruya','Meruya Selatan'
+    ,'Muara Karang','Otista','Pademangan','Palmerah','Pancoran','Pantai Indah Kapuk','Pantai Mutiara','Parung Panjang','Pasar Minggu','Pasar Rebo','Pegadungan','Pegangsaan','Pejaten','Penjaringan','Permata Buana','Permata Hijau'
+    ,'Permata Puri Media','Pesanggrahan','Petojo Utara','Petukangan','Pisangan Lama','Pluit','Plumpang','Pondok Bambu','Pondok Cabe','Pondok Gede','Pondok Indah','Pondok Kelapa'
+    ,'Pondok Kopi','Pondok Labu','Pondok Pinang','Pondok Ranggon','Pos Pengumben','Pulo Asem','Pulo Gadung','Pulo Gebang','Pulomas','Puri Indah','Puri Mansion','Radio Dalam'
+    ,'Ragunan','Rawa Belong','Rawalumbu','Rawamangun','Roxy','Salemba','Sawah Besar','Sawangan','Semper','Senen','Setia Budi','Slipi','Srengseng','Sudirman','Sunrise','Sunter','Sunter Agung'
+    ,'Supomo','Taman Cosmos','Taman Kencana','Taman Mini','Taman Modern','Taman Palem','Taman Ratu','Taman Sari','Taman Semanan Indah','Taman Surya','Tambora','Tanah Abang'
+    ,'Tanah Kusir','Tanjung Duren','Tanjung Priok','Tb. Simatupang','Tebet','Tebet Barat','Tebet Timur','Tebet Utara','Teluk Gong','Thamrin','Tomang','Ulujami','Utan Kayu','Warung Buncit']
+
+    var lines1 = ['None', 'AJB - Akta Jual Beli',"Built: 2006",'Built: 2015','Hak Pakai','Hak Sewa','PPJB - Perjanjian Pengikatan Jual Beli'
+      ,'SHGB - Hak Guna Bangunan','SHM - Sertifikat Hak Milik','Sertifikat Belum Pecah','Strata','Surat Ijo / Surat Hijau','Tanah Eigendom Verponding','Tanah Girik / Rincik / Kikitir','Warisan'];
+    
+    var lines2 = ['North','Central','East','Weast','South'];
+
     document.getElementById(myDropdown).classList.toggle("show");
 
     var line = '<input type="text" placeholder="Search ' + description1 + '" id="'+ input +'" onkeyup="filterFunction('+ "'" +myDropdown+ "'" +', '+ "'" +input+ "'" +')">';
     document.getElementById(myDropdown).insertAdjacentHTML('beforeend', line);
 
-    var lines;
+    if(myDropdown == 'myDropdown'){
 
-    fetch(path)
-    .then(response => response.txt())
-    .then(contents => {
-        lines = contents.split('\n');
-    })
-    .catch(error => {
-        console.error('Error finding file', error);
-    })
+      for(var i=0; i < lines.length; i++){
+        var button = '<button onclick="replaceName(' + "'" + lines[i] + "'" +', ' + "'" + buttonNumber + "'" + ', '+ "'" +description+ "'" +')" class="myButtons">'+ lines[i] +'</button>';
+        document.getElementById(myDropdown).insertAdjacentHTML('beforeend', button);
+      }
 
-    for(var i=0; i < lines.length; i++){
-        
+    } else if(myDropdown == 'myDropdown4'){
+
+      for(var i=0; i < lines1.length; i++){
+        var button = '<button onclick="replaceName(' + "'" + lines1[i] + "'" +', ' + "'" + buttonNumber + "'" + ', '+ "'" +description+ "'" +')" class="myButtons">'+ lines1[i] +'</button>';
+        document.getElementById(myDropdown).insertAdjacentHTML('beforeend', button);
+      }
+
+    } else if(myDropdown == 'myDropdown5'){
+
+      for(var i=0; i < lines2.length; i++){
+        var button = '<button onclick="replaceName(' + "'" + lines2[i] + "'" +', ' + "'" + buttonNumber + "'" + ', '+ "'" +description+ "'" +')" class="myButtons">'+ lines2[i] +'</button>';
+        document.getElementById(myDropdown).insertAdjacentHTML('beforeend', button);
+      }
+
     }
 
-    document.addEventListener("DOMContentLoaded", function() {
-      const button = document.getElementById("button");
-      const addressText = "Address";
-      button.textContent = addressText;
     
-      const logoImage = new Image();
-      logoImage.src = "escritorio/WebsitePrototype2/static/img/location.png";
-      logoImage.classList.add("logo-image");
-    
-      button.innerHTML = ""; // Clear the button's existing content
-      button.appendChild(logoImage); // Append the logo image to the button
-      button.appendChild(document.createTextNode(addressText)); // Append the text node to the button
-    
-      const otherDropdowns = document.querySelectorAll(".dropdown:not(#location)");
-      otherDropdowns.forEach(function(dropdown) {
-        dropdown.classList.add("no-logo");
-      });
-    });
-    
-    
-    
-    
-    
-
 
   }
   
